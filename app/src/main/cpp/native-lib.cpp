0d3a22250a9d6c657f23b68d70ce1b7258e569c5
LyricZhao <lyricz@yeah.net>
2020-03-21 17:54:18 +0800

    Add pipe reader

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,10 +7,12 @@
 // Native C++
 # include <string>
 
+// Networks
+# include <sys/socket.h>
+
 // Defines & Macros
-# define TAG "Backend"
-# define debug(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
-# define error(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+# define debug(...) __android_log_print(ANDROID_LOG_DEBUG, __func__, __VA_ARGS__)
+# define error(...) __android_log_print(ANDROID_LOG_ERROR, __func__, __VA_ARGS__)
 
 typedef unsigned char u8;
 typedef unsigned int u32;
@@ -18,17 +20,44 @@ typedef unsigned int u32;
 // Parameters
 # define DATA_MAX_LENGTH 4096
 
+# define IP_REQUEST   100
+# define IP_REPLY     101
+# define NET_REQUEST  102
+# define NET_REPLY    103
+# define HEARTBEAT    104
+
 struct Message {
-  u32 length;
+  u32 length; // includes 'length', 'type' and 'data'
   u8 type;
   u8 data[DATA_MAX_LENGTH];
 };
 
+const Message ip_request = {sizeof(u32) + sizeof(u8), IP_REQUEST};
+const Message heartbeat = {sizeof(u32) + sizeof(u8), HEARTBEAT};
+
+int send_raw(int sockfd, void* ptr, u32 length) {
+  int sent = send(sockfd, ptr, length, 0);
+  if (sent < length) {
+    error("Failed to write raw sockets");
+    return -1;
+  }
+  return sent;
+}
+
+int send_heartbeat(int sockfd) {
+  return send_raw(sockfd, (void *) &heartbeat, heartbeat.length);
+}
+
+int send_ip_request(int sockfd) {
+  return send_raw(sockfd, (void *) &ip_request, ip_request.length);
+}
+
+// APIs
 extern "C" JNIEXPORT void JNICALL
 Java_com_lyricz_a4over6vpn_MainActivity_createBackendTunnel(
         JNIEnv* env,
         jobject /* this */) {
-
+  debug("Backend starts running");
 }
 
 extern "C" JNIEXPORT void JNICALL
--- app/src/main/java/com/lyricz/a4over6vpn/MainActivity.java
+++ app/src/main/java/com/lyricz/a4over6vpn/MainActivity.java
@@ -1,8 +1,63 @@
 package com.lyricz.a4over6vpn;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+// A pipe implement only for reading
+class Pipe {
+    // Singleton
+    static int BUFFER_LENGTH = 4096;
+    static String TAG = "Java.Pipe";
+
+    static int pipe_count = 0;
+    static File directory;
+
+    // Object Oriented
+    private File file;
+    private byte[] buffer;
+
+    Pipe() {
+        pipe_count += 1;
+        int id = pipe_count;
+        file = new File(directory, "pipe_" + String.valueOf(id));
+        buffer = new byte[BUFFER_LENGTH];
+    }
+
+    public String path() {
+        return file.getAbsolutePath();
+    }
+
+    public void clean() {
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    public String read() {
+        int length = 0;
+        try {
+            FileInputStream stream = new FileInputStream(file);
+            length = (new BufferedInputStream(stream)).read(buffer);
+        } catch (Exception exception) {
+            Log.e(TAG, "Error while reading pipe " + file.getName());
+        }
+        return length > 0 ? new String(buffer) : "";
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +71,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createBackendTunnel();
-        terminateBackendTunnel();
+        checkPermissions();
+        Pipe.directory = getApplicationContext().getCacheDir();
+    }
+
+    private void checkPermissions() {
+        String[] permissions = new String[] {
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.BIND_VPN_SERVICE
+        };
+
+        List<String> needed = new ArrayList<>();
+
+        for (String permission: permissions) {
+            int granted = ContextCompat.checkSelfPermission(this, permission);
+            if (granted != PackageManager.PERMISSION_GRANTED) {
+                needed.add(permission);
+            }
+        }
+
+        if (!needed.isEmpty()) {
+            ActivityCompat.requestPermissions(this, needed.toArray(new String[0]), 0);
+        }
     }
 
     /**
